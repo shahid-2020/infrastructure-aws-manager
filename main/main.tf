da0eb51ad5fc5f108dc85658b2d7a61c22a13c3c
shahid <khanshahid9283@gmail.com>
2025-09-28 22:49:37 +0530

    chores: remove domain

--- main/main.tf
+++ main/main.tf
@@ -263,11 +263,11 @@ module "security_group_ingress_5432_ingress" {
   cidr_ipv6           = "::/0"
 }
 
-module "domain" {
-  source = "./modules/route53"
+# module "domain" {
+#   source = "./modules/route53"
 
-  name = "codlr.store"
-}
+#   name = "codlr.store"
+# }
 
 module "ecs_cluster_staging" {
   source = "./modules/ecs-cluster"
